df0fdf8c0113885cb4962b9e911d5de0753a0988
Spore Engineering <65569953+spore-engineering@users.noreply.github.com>
2022-07-08 23:01:19 -0700

    links updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,6 +11,8 @@ export default function Footer() {
             <div>
                 <span className="footer-title">Nosotros</span> 
                 <a href="https://criptomu.online" className="link link-hover" target="_blank" rel="noopener noreferrer">Sitio Oficial</a> 
+                <a href="https://foro.criptomu.online" className="link link-hover" target="_blank" rel="noopener noreferrer">Foro</a> 
+                <a href="https://gnosis-safe.io/app/matic:0x2387A9Ef973eE968d009091CF8F09A60122373B3/home" className="link link-hover" target="_blank" rel="noopener noreferrer">Multisig</a> 
             </div> 
             <div>
                 <span className="footer-title">Sociales</span> 
@@ -20,9 +22,9 @@ export default function Footer() {
             </div> 
             <div>
                 <span className="footer-title">Juego</span> 
-                <a href="#" className="link link-hover" target="_blank" rel="noopener noreferrer">Jugar</a> 
-                <a href="#" className="link link-hover" target="_blank" rel="noopener noreferrer">Cliente</a> 
-                <a href="#" className="link link-hover" target="_blank" rel="noopener noreferrer">Documentación</a>
+                <a href="https://play.criptomu.online" className="link link-hover" target="_blank" rel="noopener noreferrer">Jugar</a> 
+                <a href="https://opensea.io/collection/criptomuonline" className="link link-hover" target="_blank" rel="noopener noreferrer">OpenSea</a> 
+                <a href="https://ayuda.criptomu.online" className="link link-hover" target="_blank" rel="noopener noreferrer">Documentación</a>
             </div>
         </footer>
             </div>
